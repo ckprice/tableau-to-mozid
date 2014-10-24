1e4301459e30df906f995ac2c4a2a2c41bd1e107
Cory Price <cprice@mozilla.com>
2014-10-24 15:30:15 -0700

    Bootstrap

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import webapp2
+import os
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write('Hello, World!')
+
+        template_values = {
+            'greetings': 'greetings',
+            'url': 'url',
+            'url_linktext': 'url_linktext',
+            'title': 'Booloo Export',
+        }
+        path = os.path.join(os.path.dirname(__file__), 'templates/start.html')
+        self.response.out.write(template.render(path, template_values))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
